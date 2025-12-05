1bbced989038feac2ad4d8f75de9f38bdd6db08a
suprith <168395311+SuPrAA5@users.noreply.github.com>
2025-12-05 14:06:39 +0530

    Update Arraysub.java

--- Arraysub.java
+++ Arraysub.java
@@ -1,22 +1,24 @@
 package com.acharya.classs;
+
 import java.util.*;
+
 public class Arraysub {
 
 	public static void main(String[] args) {
-		int arr1[]= {12,13,24,53};
-		System.out.println("enter your start index");
-	    Scanner sc=new Scanner(System.in);
-	    int sindex=sc.nextInt();
-	    System.out.println("enter your end index");
-	    
-	    int eindex=sc.nextInt();
-	    
-	    Arraysub.subArray(arr,sindex,eindex);
+		int arr1[] = { 12, 13, 24, 53 };
+		System.out.println("enter your start index:");
+		Scanner sc = new Scanner(System.in);
+		int sindex = sc.nextInt();
+		System.out.println("enter your end index:");
+
+		int eindex = sc.nextInt();
+
+		int a = Arraysub.subArray(arr1, sindex, eindex);
 
 	}
 
-	public static int subArray(int myarr[],int sindex,int eindex) {
-		for (int i = eindex; i <eindex; i++) {
+	public static int subArray(int myarr[], int sindex, int eindex) {
+		for (int i = sindex; i < eindex; i++) {
 			System.out.println(myarr[i]);
 
 		}
@@ -24,4 +26,3 @@ public class Arraysub {
 	}
 
 }
-
